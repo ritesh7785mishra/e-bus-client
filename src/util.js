e11b01bd14a88df2ab3d72e2f564185501c8c80f
unknown <ritesh7785mishra@gmail.com>
2023-04-29 13:55:40 +0530

    server url updated

--- src/util.js
+++ src/util.js
@@ -1,3 +1,3 @@
-export const baseServerUrl = "http://localhost:3000";
+export const baseServerUrl = "https://e-savari.onrender.com";
 export const apiKey = "BDPPjyvXBVAzxOAGcWRV98qIHP978rko";
 export const adminKey = "JDKUpg4BO1ZDPfGSrqG0QlbrmBYOhlfwDw0WlrCytJUo5IyO";
